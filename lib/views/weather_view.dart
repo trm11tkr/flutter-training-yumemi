505875663be6ca99b4b106e4a89abbbb0d69d681
trm11tkr <mrmrt1121@gmail.com>
2022-11-15 10:37:44 +0900

    tmporary weather view

---
+++ lib/views/weather_view.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class WeatherView extends StatelessWidget {
+  const WeatherView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: deviceWidth / 2,
+              child: Column(
+                children: [
+                  const AspectRatio(
+                    aspectRatio: 1,
+                    child: Placeholder(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            '** ℃',
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge
+                                ?.copyWith(
+                                  color: Colors.blue,
+                                ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Text(
+                            '** ℃',
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge
+                                ?.copyWith(
+                                  color: Colors.red,
+                                ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            SizedBox(
+              width: deviceWidth / 2,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text('Close'),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text('Reload'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
