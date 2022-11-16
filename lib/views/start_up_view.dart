f790bda21f4887967e2d4c0eed56e2bb3f293e50
trm11tkr <mrmrt1121@gmail.com>
2022-11-16 13:23:10 +0900

    implement automatic transition loops

--- lib/views/start_up_view.dart
+++ lib/views/start_up_view.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:flutter_training/utils/logger.dart';
 import 'package:flutter_training/views/weather_view.dart';
-import 'package:yumemi_weather/yumemi_weather.dart';
 
 class StartUpView extends StatefulWidget {
   const StartUpView({super.key});
@@ -12,131 +9,24 @@ class StartUpView extends StatefulWidget {
 }
 
 class _StartUpViewState extends State<StartUpView> {
-  final _weatherClient = YumemiWeather();
-
-  SvgPicture? _currentWeather;
-
-  @override
-  void initState() {
-    WidgetsBinding.instance.endOfFrame.then((_) {
-      Navigator.of(context).push(
-        MaterialPageRoute<void>(
-          builder: (context) => const WeatherView(),
-        ),
-      );
-    });
-    super.initState();
-  }
-
-  String? _fetchWeather(YumemiWeather client) {
-    try {
-      final weather = client.fetchSimpleWeather();
-      return weather;
-    } on YumemiWeatherError catch (e) {
-      logger.shout(e);
-      return null;
-    }
-  }
-
-  SvgPicture _weatherToImage(String weather) {
-    return SvgPicture.asset('assets/images/$weather.svg');
-  }
-
   @override
   Widget build(BuildContext context) {
-    final deviceWidth = MediaQuery.of(context).size.width;
-
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            const Spacer(),
-            SizedBox(
-              width: deviceWidth / 2,
-              child: Column(
-                children: [
-                  AspectRatio(
-                    aspectRatio: 1,
-                    child: _currentWeather ?? const Placeholder(),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Text(
-                            '** ℃',
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelLarge
-                                ?.copyWith(
-                                  color: Colors.blue,
-                                ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Text(
-                            '** ℃',
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelLarge
-                                ?.copyWith(
-                                  color: Colors.red,
-                                ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  SizedBox(
-                    width: deviceWidth / 2,
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Center(
-                            child: TextButton(
-                              onPressed: () {},
-                              child: const Text('Close'),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Center(
-                            child: TextButton(
-                              onPressed: () {
-                                final weather = _fetchWeather(_weatherClient);
-                                if (weather == null) {
-                                  return;
-                                } else {
-                                  setState(() {
-                                    _currentWeather = _weatherToImage(weather);
-                                  });
-                                }
-                              },
-                              child: const Text('Reload'),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    // Widgetの描画が完了するまで待機
+    WidgetsBinding.instance.endOfFrame.then((_) async {
+      // 500ミリ秒待機
+      await Future<void>.delayed(
+        const Duration(milliseconds: 500),
+      ).then((_) async {
+        // WeatherView に遷移
+        await Navigator.of(context).push(
+          MaterialPageRoute<void>(
+            builder: (context) => const WeatherView(),
+          ),
+        );
+        // pop されると再描画
+        setState(() {});
+      });
+    });
+    return const Scaffold();
   }
 }
