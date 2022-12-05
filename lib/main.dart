7ad081d9fbda5d4f6315596d305f9a1be284b2b9
trm11tkr <mrmrt1121@gmail.com>
2022-12-05 18:14:10 +0900

    fixed to portrait screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_training/app.dart';
 import 'package:flutter_training/utils/logger.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  //向き指定
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, //縦固定
+  ]);
+
   // Logger初期化
   Logger.configure();
   runApp(
