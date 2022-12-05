675220c52b1806f335989e1a520df2b52918c3d0
trm11tkr <mrmrt1121@gmail.com>
2022-12-05 11:44:37 +0900

    separate App class

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_training/app.dart';
 import 'package:flutter_training/utils/logger.dart';
-import 'package:flutter_training/views/start_up_view.dart';
 
 void main() {
   // Logger初期化
   Logger.configure();
   runApp(
     const ProviderScope(
-      child: MyApp(),
+      child: App(),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const StartUpView(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
 }
